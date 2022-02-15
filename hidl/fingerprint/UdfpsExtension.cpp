edff00807124275273999d54985a2c00b562cdbb
LuK1337 <priv.luk@gmail.com>
2022-02-15 09:14:16 +0100

    common: fingerprint: Fix sde_drm.h include path on 5.4+ kernel
    Change-Id: Idac59471ae329eabf8484cc1238c35dabaa64827

--- hidl/fingerprint/UdfpsExtension.cpp
+++ hidl/fingerprint/UdfpsExtension.cpp
@@ -4,7 +4,12 @@
  */
 
 #include <compositionengine/UdfpsExtension.h>
+
+#if __has_include(<display/drm/sde_drm.h>)
+#include <display/drm/sde_drm.h>
+#elif __has_include(<drm/sde_drm.h>)
 #include <drm/sde_drm.h>
+#endif
 
 #if __has_include(<display/drm/sde_drm.h>)
 #include <display/drm/sde_drm.h>
